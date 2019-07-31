a9f0e376e3b85fde67ddd4f86b1fa3f333c39c77
vijayaraman267 <vijayaraman.sivam@gmail.com>
2019-07-31 13:28:04 +0530

    First commit

---
+++ EmployeeService1/App_Start/WebApiConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+
+namespace EmployeeService1
+{
+    public static class WebApiConfig
+    {
+        //public class CustomJsonFormatter : JsonMediaTypeFormatter
+        //{
+        //    public CustomJsonFormatter()
+        //    {
+        //        this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        //    }
+        //    public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        //    {
+        //        base.SetDefaultContentHeaders(type, headers, mediaType);
+        //        headers.ContentType = new MediaTypeHeaderValue("application/json");
+        //    }
+        //}
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //Custom Formater
+           // config.Formatters.Add(new CustomJsonFormatter());
+
+            // Restrict XML only allow json format
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("test/html"));
+
+            //Restrict Json only allow Xml format
+            //config.Formatters.Remove(config.Formatters.JsonFormatter);
+        }
+    }
+}
---
+++ EmployeeService1/Controllers/EmployeeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeAccess;
+
+namespace EmployeeService1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (DemoEntities entity = new DemoEntities())
+            {
+                return entity.Employees.ToList();
+            }
+        }
+
+
+        public HttpResponseMessage Get(int id)
+        {
+            using (DemoEntities entity = new DemoEntities())
+            {
+                var Result = entity.Employees.FirstOrDefault(e => e.ID == id);
+                if (Result != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, Result);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "Employee With Id="+id.ToString() + "Not found");
+                }
+                //return entity.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+
+        public HttpResponseMessage Post([FromBody]Employee employee)
+        {
+            try
+            {
+                using (DemoEntities entity = new DemoEntities())
+                {
+                    entity.Employees.Add(employee);
+                    entity.SaveChanges();
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+
+                //throw;
+            }
+            
+        }
+
+         
+     
+
+         
+    }
+}
